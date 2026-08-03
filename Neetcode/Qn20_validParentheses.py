8509b3562e58403ea914e452945eb112d32c4fe2
Mingyuan Zhang <e0970135@u.nus.edu>
2026-08-03 21:18:28 +0800

    valid parenthesis

---
+++ Neetcode/Qn20_validParentheses.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pairs = {
+            "(" : ")",
+            "[" : "]",
+            "{" : "}"
+        }
+
+        stack = []
+
+        for char in s:
+            if char in "({[":
+                stack.append(char)
+            
+            else:
+                if not stack:
+                    return False
+                
+                top = stack.pop()
+
+                if pairs[top] != char:
+                    return False
+
+        return len(stack) == 0
+
+
+                
+                
